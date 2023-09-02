85f46bdd8d22c619d1081dd5d37a55af94ba05ad
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-09-02 16:09:39 +0600

    header part complete

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-lime-600 justify-between text-white flex py-2 px-10 items-center'>
+            <p className="font-bold">Knowledge Cafe</p>
+            <button className="bg-white text-lime-700 rounded px-9 py-2">Login</button>
+        </div>
+    );
+};
+
+export default Header;
